6156d1dd77a5cac4910e86934ca65d6450877d25
Morten Schioldan <mortens@microsoft.com>
2019-05-17 19:35:41 +0200

    sds

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world 2\n');
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running...`);
